0abdf57cfa7ed4344ec51447cb765743951517fd
Nestor Garcia <nestorjgcc@hotmail.com>
2023-04-05 23:37:10 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <p class="red">Hello {{ firstName }}!</p>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      firstName: "Nestor",
+    };
+  },
+};
+</script>
+
+<style>
+.red {
+  color: blue;
+}
+</style>
